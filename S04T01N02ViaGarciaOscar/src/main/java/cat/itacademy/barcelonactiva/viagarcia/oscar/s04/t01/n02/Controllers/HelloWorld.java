d34b25b29eb9c31527465942b3d26b985a02a41e
Oscar <oscar.via@gmail.com>
2022-07-01 09:13:44 +0200

    Primra Tamesa Nivell1 i Nivell2

---
+++ S04T01N02ViaGarciaOscar/src/main/java/cat/itacademy/barcelonactiva/viagarcia/oscar/s04/t01/n02/Controllers/HelloWorld.java
@@ -0,0 +1,25 @@
+package cat.itacademy.barcelonactiva.viagarcia.oscar.s04.t01.n02.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@GetMapping("/HelloWorld")
+	public static String saluda(@RequestParam(required = false, defaultValue = "UNKNOWN")  String nom) {
+		return "Hola, "+ nom + ". Estàs executant un projecte Maven";
+			
+	}
+	
+	@GetMapping(value ={"/HelloWorld2", "/HelloWorld2/{nom}"})
+	public String saluda2(@PathVariable(required = false) String nom) {
+		
+		if (nom == null ) {nom = "UNKNOWN";};
+		return "Hola, "+ nom + ". Estàs executant un projecte Maven";
+			
+	}
+
+}
